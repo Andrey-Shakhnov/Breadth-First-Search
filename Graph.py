5675a77934b99551a2292b0a5c195dc96a7a55d3
Andrey <avshakhnov@gmail.com>
2019-12-15 16:44:49 +0300

    Update previous version

--- Graph.py
+++ Graph.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import time
 import pylab as plt
+import os
 
 flag = 0
 while (flag == 0):
@@ -9,7 +10,7 @@ while (flag == 0):
 
 	#Генерируем рандомный граф
 	G = nx.fast_gnp_random_graph(n,p)
-
+	print(G)
 	f = open('time.txt', 'w')
 	#Выполняем поиск в ширину и считаем время, за которое он производится
 	root = 0
@@ -17,35 +18,51 @@ while (flag == 0):
 	edges = nx.bfs_edges(G,root)
 	nodes = [root] + [v for u, v in edges]
 	times = time.process_time()
-	print(time.process_time()," seconds")
+	print("BFS time is: ",time.process_time()," seconds")
 	f.write(str(times))
 	f.close()
 	
 	#Вывести путь поиска в ширину?
-	print("Do you want to see BFS way?")
-	key1 = input("y/n? \n")
-	if (key1 == "y"):
-		print(nodes)
-	
+	if (n <= 20):
+		print("The BFS way is: \n",nodes)
+	else:
+		print("Do you want to see BFS way?")
+		key1 = input("y/n? \n")
+		if (key1 == "y"):
+			print(nodes)
+
 	if (n <=15):
 		nx.draw(G, with_labels = True)
 		plt.savefig("plot.png")
 		plt.show()
+	print("input K,T")
+	k = int(input())
+	t = int(input())
+	G.add_edges_from([(k, t)]) #Добавляем ребро от вершины k к вершине t.
 	print("==========================================")
 	#Кратчайшие пути: от введенной вершины до всех остальных и от нулевой до введённой
 	start = 0 #int(input("Enter start vertex: "))
 	end = int(input("Enter target: "))
-	p = nx.shortest_path(G,end)
+	p = nx.shortest_path(G,source = 0, target = end)
 	print(p)
 	
-	np = nx.bidirectional_shortest_path(G,start,end)
-	print(np)
-
+	#np = nx.bidirectional_shortest_path(G,start,end)
+	#print(np)
+	f = open('degree.txt', 'w')
+	text1 = 'Degree '
+	text2 = ' vertex is '
 	tmp = 0
 	d = [] * n
 	for i in range(n):
-		print("Degree",i,"vertex is",G.degree[i])
+	#	print("Degree",i,"vertex is",G.degree[i])
+		out = text1 + str(i) + text2 + str(G.degree[i]) + '\n'
+		f.write(out)
 		d.append(G.degree[i])
+	f.close()
+	print("To open file with vertex's degrees?")
+	keyd = input("y/n? ")
+	if (keyd == "y"):
+		os.system('degree.txt')
 	d = sorted(d)
 	print("Max degree - ",max(d))
 
